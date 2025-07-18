4ba7ef98656f4ff289e3e3719a3dcbd5706b3967
MOHAN RAJ S <131248839+Mohanraj-11@users.noreply.github.com>
2025-07-18 12:21:52 +0530

    Update Saucedemo.java

--- Saucedemo.java
+++ Saucedemo.java
@@ -1,21 +1,29 @@
 package sauceDemo;
 
+// Import necessary Selenium classes
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Saucedemo {
-	
-	public static void main(String[] args) {
-		
-		ChromeDriver driver =new ChromeDriver();
-		driver.get("https://www.saucedemo.com/v1/");
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-		driver.findElement(By.id("login-button")).click();
-		
-		driver.quit();
-		
-	
-	}
 
+    public static void main(String[] args) {
+
+        // Launch a new Chrome browser instance
+        ChromeDriver driver = new ChromeDriver();
+
+        // Open the SauceDemo login page
+        driver.get("https://www.saucedemo.com/v1/");
+
+        // Enter the username "standard_user" in the username field
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+        // Enter the password "secret_sauce" in the password field
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        // Click on the login button
+        driver.findElement(By.id("login-button")).click();
+
+        // Close the browser
+        driver.quit();
+    }
 }
